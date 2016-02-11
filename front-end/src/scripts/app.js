5d163f8257a671527008822ec4f41c2126dd6a3c
dophamine <dophaminedev@gmail.com>
2016-02-11 14:51:27 +0300

    decor elements, effects, input menu

--- front-end/src/scripts/app.js
+++ front-end/src/scripts/app.js
@@ -6,9 +6,9 @@ $(document).ready(function() {
 	$(tglPanel).on('click', function(){
 		$(this).toggleClass('active');
 		if ($(this).hasClass('active')) {
-			$(this).html('>');
+			$(this).removeClass("arrow-right").addClass("arrow-left");
 		} else {
-			$(this).html('<');
+			$(this).removeClass("arrow-left").addClass("arrow-right");
 		}
 		$(panel).toggleClass('hidden');
 	});
