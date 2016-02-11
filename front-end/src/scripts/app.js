7b7ecfa038afab185f9140a528a894db1e0f30e4
dophamine <dophaminedev@gmail.com>
2016-02-11 16:59:08 +0300

    custom input menu, add interactivity

--- front-end/src/scripts/app.js
+++ front-end/src/scripts/app.js
@@ -1,6 +1,9 @@
 $(document).ready(function() {
 	var tglPanel = document.getElementById('toggle-panel');
 	var panel = document.getElementById('panel');
+	var metroInput = document.getElementById('metro-input');
+	var metroList = document.getElementById('metro-list');
+
 
 	// Toggle panel
 	$(tglPanel).on('click', function(){
@@ -12,4 +15,31 @@ $(document).ready(function() {
 		}
 		$(panel).toggleClass('hidden');
 	});
+
+	// Toggle list
+	$('.input-menu .arrow').on('click', function() {
+		$('#metro-list').toggleClass('show');
+		
+		if ($(this).hasClass('active')) {
+			$(this).removeClass("arrow-top").addClass("arrow-bottom");
+		} else {
+			$(this).removeClass("arrow-bottom").addClass("arrow-top");
+		}
+		$(this).toggleClass('active');
+	});
+
+	//input-menu. add items
+	$('#metro-list .item').click(function() {
+		var value = $(this).text();
+		var temp = $(metroInput).html();
+		$(metroInput).html(temp + "<span class='cross' contenteditable='false'>" + value+ "</span>");
+		$('#metro-input span').click(function(){
+			$(this).remove();
+		});
+	});
+
+	$('#metro-input span').click(function(){
+		$(this).remove();
+	});
+
 });
