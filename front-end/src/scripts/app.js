c089c94b907ef561aee4016c8bea872f015f7c72
dophamine <dophaminedev@gmail.com>
2016-02-13 23:14:01 +0300

    big commit with many bugs :D

--- front-end/src/scripts/app.js
+++ front-end/src/scripts/app.js
@@ -20,8 +20,130 @@ $(document).ready(function() {
 	var mainInput = document.getElementById('main-input');
 	var List = document.getElementById('list');
 	var txt = document.getElementById('txt');
+	var routes = $('#routes');
 	var listArrow = $('.input-menu .arrow');
 	var noMatches = false;
+	var detailedRoute;
+
+	var json = {
+	    "Автово" : {
+	        "routes" : {
+	            "К 145" : [
+	            	"Славы проспект (ул. Софийская)",
+					"Софийская д.40",
+					"Софийская д.43",
+					"Белы Куна (ул. Софийская)",
+					"Белы Куна (ул. Пражская)",
+					"Метро Международная"
+	            ],
+	            "К 146" : [
+	            	"Славы проспект (ул. Софийская)",
+					"Софийская д.40",
+					"Софийская д.43",
+					"Белы Куна (ул. Софийская)",
+					"Белы Куна (ул. Пражская)",
+					"Метро Международная"
+	            ],
+	            "К 14" : [
+	            	"Славы проспект (ул. Софийская)",
+					"Софийская д.40",
+					"Софийская д.43",
+					"Белы Куна (ул. Софийская)",
+					"Белы Куна (ул. Пражская)",
+					"Метро Международная"
+	            ],
+	            "К 148" : [
+	            	"Славы проспект (ул. Софийская)",
+					"Софийская д.40",
+					"Софийская д.43",
+					"Белы Куна (ул. Софийская)",
+					"Белы Куна (ул. Пражская)",
+					"Метро Международная"
+	            ],
+	            "К 135" : [
+	            	"Славы проспект (ул. Софийская)",
+					"Софийская д.40",
+					"Софийская д.43",
+					"Белы Куна (ул. Софийская)",
+					"Белы Куна (ул. Пражская)",
+					"Метро Международная"
+	            ],
+	            "145" : [
+	            	"Славы проспект (ул. Софийская)",
+					"Софийская д.40",
+					"Софийская д.43",
+					"Белы Куна (ул. Софийская)",
+					"Белы Куна (ул. Пражская)",
+					"Метро Международная"
+	            ]
+	        }
+	    },
+	    "Адмиралтейская": {
+	        "routes" : {
+	            "К 65" : [
+	            	"Славы проспект (ул. Софийская)",
+					"Софийская д.40",
+					"Софийская д.43",
+					"Белы Куна (ул. Софийская)",
+					"Белы Куна (ул. Пражская)",
+					"Метро Международная"
+	            ],
+	            "К 46" : [
+	            	"Славы проспект (ул. Софийская)",
+					"Софийская д.40",
+					"Софийская д.43",
+					"Белы Куна (ул. Софийская)",
+					"Белы Куна (ул. Пражская)",
+					"Метро Международная"
+	            ],
+	            "К 4" : [
+	            	"Славы проспект (ул. Софийская)",
+					"Софийская д.40",
+					"Софийская д.43",
+					"Белы Куна (ул. Софийская)",
+					"Белы Куна (ул. Пражская)",
+					"Метро Международная"
+	            ],
+	            "К 48" : [
+	            	"Славы проспект (ул. Софийская)",
+					"Софийская д.40",
+					"Софийская д.43",
+					"Белы Куна (ул. Софийская)",
+					"Белы Куна (ул. Пражская)",
+					"Метро Международная"
+	            ],
+	            "К 35" : [
+	            	"Славы проспект (ул. Софийская)",
+					"Софийская д.40",
+					"Софийская д.43",
+					"Белы Куна (ул. Софийская)",
+					"Белы Куна (ул. Пражская)",
+					"Метро Международная"
+	            ],
+	            "45" : [
+	            	"Славы проспект (ул. Софийская)",
+					"Софийская д.40",
+					"Софийская д.43",
+					"Белы Куна (ул. Софийская)",
+					"Белы Куна (ул. Пражская)",
+					"Метро Международная"
+	            ]
+	        }
+	    }
+	};
+
+	var newjson = JSON.parse(JSON.stringify(json));
+
+	// var data = [
+	// 	"Славы проспект (ул. Софийская)",
+	// 	"Софийская д.40",
+	// 	"Софийская д.43",
+	// 	"Белы Куна (ул. Софийская)",
+	// 	"Белы Куна (ул. Пражская)",
+	// 	"Метро Международная"
+	// ];
+
+	var inputItems = [];
 
 	//spans in input
 	var spans = [];
@@ -56,6 +178,18 @@ $(document).ready(function() {
 		}
 	});
 
+	function generateRouteSection (key) {
+		var body, list ='';
+		for (var i in newjson[key].routes) {
+			list += '<li class="route">\n\t<span class="route-title">' + i + '</span>\n<div class="detail"><a href="#" class="detail-txt">Подробнее</a></div>\n</li>';
+		}
+		body = '<div class="route-section">
+					<h2 class="district-title">' + key + ':</h2>
+						<ul class="route-list">\n' + list + '</ul></div>';
+		$(routes).append(body);
+		$('.route-title').on('click', routehandler);
+	}
+
 	//input-menu. add items to input
 	function listBindEvt () {
 		$('#list .item').on('click', function() {
@@ -65,12 +199,16 @@ $(document).ready(function() {
 			if (inArr(spans, value) === -1) {
 				spans.push(value);
 				$(txt).before("<span class='rc'>" + value + "<i class='cross'></i></span>");
+				generateRouteSection(value);
 			}
+
+			// inputItems.push($('#main-input span:nth-last-child(2)'));
 
 			// remove items
 			$('#main-input span:nth-last-child(2) > .cross').on('click', function() {
 				var value = $(this).parent('span').text();
 				$(this).parent('span').remove();
+				$('.district-title:contains('+ value +':)').closest('.route-section').remove();
 				spanClean();
 			});
 		});
@@ -105,16 +243,10 @@ $(document).ready(function() {
 
 	function search(term, suggest){
 		term = term.toLowerCase();
-		var choices = [
-			'Автово', 
-			'Адмиралтейская', 
-			'Академическая',
-			'Балтийская',
-			'Бухарестская',
-			'Василеостровская',
-			'Владимирская',
-			'Старая деревня'
-		];
+		var choices = [];
+		for (var i in newjson) {
+			choices.push(i);
+		}
 		var matches = [];
 		for (var i=0; i<choices.length; i++)
 			if (~choices[i].toLowerCase().indexOf(term)) matches.push(choices[i]);
@@ -122,7 +254,6 @@ $(document).ready(function() {
 
 		//toggle list if matches not found
 		noMatches = matches.length > 0 ? false : true;
-		
 	}
 
 	$('#txt').on('input propertychange', function() {
@@ -146,14 +277,85 @@ $(document).ready(function() {
 		return '<li class="item" data-val="' + item + '">' + item.replace(re, "<b>$1</b>") + '</li>';
 	}
 
+	// scroll
 	$('.panel-wrap').mCustomScrollbar({
 		theme:"minimal-dark",
 		autoHideScrollbar: true
 	});
+	
 
-	$('#stop-list').mCustomScrollbar({
-		theme:"minimal-dark",
-		autoHideScrollbar: true
-	});
+	//toggle route
+	// $('.route-title').bind('click', routehandler(e));
+
+	function routehandler () {
+		var route = $(this).closest('.route');
+		var a = $('.detail-txt', route);
+		var title = route.closest('.route-section').children('.district-title').text();
+		title = title.substring(0, title.length - 1);
+		var data = newjson[title].routes[$(this).text()];
+		console.log(data);
+		
+		//toggle route detail
+		$(a).one('click', function(e) {
+			e.preventDefault();
+
+			$(route).toggleClass('detailed');
+
+			if (!detailedRoute) {
+				console.log('1');
+				detailedRoute = route;
+				renderContext(data, route);
+			} else if (detailedRoute.get(0) === route.get(0)) {
+				console.log('2');
+				if (route.hasClass('detailed')) {
+					detailedRoute = route;
+					$('.context').remove();
+					renderContext(data, route);
+				} else {
+					detailedRoute = null;
+					$('.context').remove();
+					console.log('bug');
+				}
+			} else {
+				console.log('3');
+				detailedRoute.removeClass('detailed');
+				detailedRoute = route;
+				$('.context').remove();
+				renderContext(data, route);
+			}
+
+
+			$('#routes .vars').mCustomScrollbar({
+				theme:"minimal-dark",
+				autoHideScrollbar: true
+			});
+		});
+
+		route.toggleClass('active');
+		if (route.hasClass('detailed')) {
+			route.toggleClass('detailed active');
+			$('.context').remove();
+		}
+	}
+
+	//rendering Route context
+	function renderContext (data, target) {
+		if (!((typeof data == "object") && (data instanceof Array))) return '';
+		var list = '';
+		var body = '';
+		for (var i = 0; i < data.length; i++) {
+			list += '<li class="item">' + data[i]+ '</li> \n';
+		}
+		body = '<div class="context"><div class="context-head"><div class="end">' + data[0] + ' - ' + data[data.length -1] + '</div><div class="arrow arrow-bottom"></div></div><ul id="stop-list" class="vars">' + list + '</ul></div>';
+		var parent = $(target).closest('.route-list');
+		var numberOfChildren = $(parent).children('.route').length;
+		var targetPos = $(target).closest('.route-list').children('.route').index($(target)) + 1;
+		var pos = Math.ceil(targetPos / 4) * 4;
+		if (pos === Math.ceil(numberOfChildren / 4) * 4) {
+			$(parent).append(body);
+		} else {
+			$(parent).children('.route').eq(pos - 1).after(body);
+		}
+	}
 
 });
